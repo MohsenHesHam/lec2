06afe45fafb218ca54ae6c81c92ee11f20bad055
Mohsen Hesham <139225619+MohsenHesHam@users.noreply.github.com>
2025-09-02 11:32:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lec1/',   // 👈 add this line
 })
